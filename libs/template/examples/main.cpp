a84c587d7b765c21a554d9cc5fb6cbe86736a6f9
Maxime Boucher <mxboucher@gmail.com>
2025-04-29 21:09:32 -0700

    init

---
+++ libs/template/examples/main.cpp
@@ -0,0 +1,7 @@
+#include "lib.hpp"
+#include <iostream>
+
+int main() {
+	auto x = add(1, 2);
+	std::cout << x << std::endl;
+}
---
+++ libs/template/include/lib.hpp
@@ -0,0 +1,6 @@
+#ifndef CPP_LIB_HPP
+#define CPP_LIB_HPP
+
+int add(int a, int b);
+
+#endif //CPP_LIB_HPP
